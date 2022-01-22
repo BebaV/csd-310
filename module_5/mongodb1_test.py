1ea6d9200677c29a880c21793e9c3d36eb70788d
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-22 17:17:51 -0600

    trial to make it work right
    t

--- module_5/mongodb1_test.py
+++ module_5/mongodb1_test.py
@@ -1,6 +1,6 @@
-import pymongo
+from pymongo import MongoClient
 
-url="mongodb+srv://admin:admin@cluster0.69vpp.mongodb.net/pytech?retryWrites=true&w=majority"
-client=pymongo.MongoClient(url)
-db=client.pytech
-print(db.list_collection_names)
+url = "mongodb+srv://admin:admin@cluster0.vy4xh.mongodb.net/pytech?retryWrites=true&w=majority0"
+client = MongoClient(url)
+db = client.pytech
+print(db.list_collection_names())
