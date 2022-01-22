f6d308ef48b6b2cfaf8cbeb1eddcee252975009f
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-22 17:29:15 -0600

    3

--- module_5/mongodb1_testb.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-
-url="mongodb+srv://admin:admin@cluster0.69vpp.mongodb.net/pytech?retryWrites=true&w=majority"
-client=pymongo.MongoClient(url)
-db=client.pytech
-print(db.list_collection_names)
