9d9a269b85249a06c2ea22b3aebe5e3c7c2ee035
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-22 17:20:31 -0600

    Create pytech_insert.py
    insert student names

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,19 @@
+MongoDB: insert_one ()
+frank = {
+    "student_id" : "1007",
+    "first_name" : " frank ",
+    "last_name" : "sinatra"
+},
+gene = {
+    "student_id" : "1008",
+    "first_name" : "gene",
+    "last_name" : "kelly"
+}, 
+judy = {
+    "student_id" : "1009",
+    "first_name" : "judy"
+    "last_name" : "garland"
+}
+frank_student_id = students.insert_one(frank).inserted_id
+gene_student_id = students.insert_one(gene).inserted_id
+judy_student_id = students.insert_one(judy).inserted_id
