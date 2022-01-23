c3ac789d2d33aeb1afc70fe10439987dee9a56cb
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-23 15:52:17 -0600

    Update mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -4,5 +4,6 @@ url = "mongodb+srv://admin:admin@cluster0.vy4xh.mongodb.net/pytech?retryWrites=t
 client = MongoClient(url)
 db = client.pytech
 
+#print formatting for presentation
 print('-- pytech collections list --')
 print(db.list_collection_names())
