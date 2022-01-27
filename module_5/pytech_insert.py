e4de2226cfe0a6317eb82d203402d9c39e51f61c
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-26 19:03:27 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,22 +1,13 @@
 from pymongo.mongo_client import MongoClient
 
-url = "mongodb+srv://admin:admin@cluster0.vy4xh.mongodb.net/pytech?retryWrites=true&w=majority0"
+url = "mongodb+srv://admin:admin@cluster0.vy4xh.mongodb.net/pytech?retryWrites=true"
 client = MongoClient(url)
 db = client.pytech
 students = db.students
 
-frank = {
-    "student_id" : "1007",
-    "first_name" : " frank ",
-    "last_name" : "sinatra"},
-gene = {
-   "student_id" : "1008",
-   "first_name" : "gene",
-   "last_name" : "kelly"},
-judy = {
-   "student_id" : "1009",
-   "first_name" : "judy",
-   "last_name" : "garland"},
+frank = {"student_id" : "1007","first_name" : " frank ","last_name" : "sinatra"}
+gene = {"student_id" : "1008","first_name" : "gene","last_name" : "kelly"}
+judy = {"student_id" : "1009","first_name" : "judy","last_name" : "garland"}
 
 frank_student_id = students.insert_one(frank).inserted_id
 gene_student_id = students.insert_one(gene).inserted_id
