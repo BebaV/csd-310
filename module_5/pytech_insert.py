d079d0e42d8ef1d4133adb5e8d77bacd2fc1ba9d
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-26 19:05:06 -0600

    added comments to the code

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,18 +1,22 @@
 from pymongo.mongo_client import MongoClient
 
+#connect to mongodb
 url = "mongodb+srv://admin:admin@cluster0.vy4xh.mongodb.net/pytech?retryWrites=true"
 client = MongoClient(url)
 db = client.pytech
 students = db.students
 
+#insert documents for the following students
 frank = {"student_id" : "1007","first_name" : " frank ","last_name" : "sinatra"}
 gene = {"student_id" : "1008","first_name" : "gene","last_name" : "kelly"}
 judy = {"student_id" : "1009","first_name" : "judy","last_name" : "garland"}
 
+#generate ID
 frank_student_id = students.insert_one(frank).inserted_id
 gene_student_id = students.insert_one(gene).inserted_id
 judy_student_id = students.insert_one(judy).inserted_id
 
+#print IDs for each of the records added
 print("-- insert statements --")
 print("inserted student record frank sinatra into the students collection with student id ", frank_student_id)
 print("inserted student record gene kelly into the students collection with student id ", gene_student_id)
