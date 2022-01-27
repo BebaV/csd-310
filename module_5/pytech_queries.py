5e36181f122e654908aab742fd90b7f9f48a288a
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-26 20:36:58 -0600

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -6,12 +6,11 @@ client = MongoClient(url)
 db = client.pytech
 students = db.students
 
-#students.find()
-#docs = db.students_name.find({})
-
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 for doc in students.find():
-    print(doc)
+    print("Student ID:",doc["student_id"])
+    print("First Name:",doc["first_name"])
+    print("Last Name:",doc["last_name"],"\n")
 
 students.find_one()
 doc = students.find_one({"student_id": "1007"})
@@ -19,4 +18,4 @@ doc = students.find_one({"student_id": "1007"})
 print("-- DISPLAYING STUDENTS DOCUMENTS FROM find_one() QUERY --")
 print("Student ID:",doc["student_id"])
 print("First Name:",doc["first_name"])
-print("Last Name:",doc["last_name"])
+print("Last Name:",doc["last_name"],"\n")
