d68b8b2fba3681275b90cea980c81dd370bd76a1
BebaV <88263181+BebaV@users.noreply.github.com>
2022-01-28 17:31:43 -0600

    Update pytech_delete.py

--- Module_6/pytech_delete.py
+++ Module_6/pytech_delete.py
@@ -6,17 +6,33 @@ client = MongoClient(url)
 db = client.pytech
 students = db.students
 
-db.collection_name.find()
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for doc in students.find():
+    print("Student ID:",doc["student_id"])
+    print("First Name:",doc["first_name"])
+    print("Last Name:",doc["last_name"],"\n")
 
-students.insert()
-joan = {
-    "student_id" : "1010",
-    "first_name" : " joan",
-    "last_name" : "crawford"
-}
+joan = {"student_id" : "1010","first_name" : " joan","last_name" : "crawford"}
+joan_student_id = students.insert_one(joan).inserted_id
+
+print("-- Insert Statements --")
+print("Inserted student record frank sinatra into the students collection with document_id ", joan_student_id,"\n")
+
 db.students.find({"student_id" : "1010"})
 
+students.find_one()
+doc = students.find_one({"student_id": "1010"})
 
-db.collection_name.remove({"student_id" : "1010"})
+print("-- DISPLAYING STUDENT TEST DOC --")
+print("Student ID:",doc["student_id"])
+print("First Name:",doc["first_name"])
+print("Last Name:",doc["last_name"],"\n")
 
-db.collection_name.find()
+students.delete_one({"student_id":"1010"})
+#db.collection_name.remove({"student_id" : "1010"})
+
+print("-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for doc in students.find():
+    print("Student ID:",doc["student_id"])
+    print("First Name:",doc["first_name"])
+    print("Last Name:",doc["last_name"],"\n")
